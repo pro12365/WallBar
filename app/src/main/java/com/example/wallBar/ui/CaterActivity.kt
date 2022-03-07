1e006fecf0f9a71e33f49c00eee36c42fee337d2
Priyanshu <priyanshurouth@gmail.com>
2022-03-07 23:19:14 +0530

    Download Feature Update

---
+++ app/src/main/java/com/example/wallBar/ui/CaterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wallBar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wallBar.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding
+        setContentView(R.layout.activity_category)
+    }
+}
---
+++ app/src/main/java/com/example/wallBar/ui/ClickedWallpaper.kt
@@ -0,0 +1,11 @@
+package com.example.wallBar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClickedWallpaper : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clicked_wallpaper)
+    }
+}
